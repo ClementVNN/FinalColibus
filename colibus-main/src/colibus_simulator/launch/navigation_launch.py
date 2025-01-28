04357daffd897858607d6cb40924a50e48432c7c
ClementVNN <clement.venien@estaca.eu>
2025-01-28 13:12:11 +0100

    Add files via upload

--- colibus-main/src/colibus_simulator/launch/navigation_launch.py
+++ colibus-main/src/colibus_simulator/launch/navigation_launch.py
@@ -8,7 +8,7 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.sss
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
@@ -25,7 +25,7 @@ from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('articubot_one')
+    bringup_dir = get_package_share_directory('colibus_simulator')
 
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
